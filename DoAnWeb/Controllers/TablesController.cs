1e9371f269759a40bcd59d59071f84515dc739f8
nguyễn hoàng <hoangsino2002@gmail.com>
2023-06-09 23:10:29 +0700

    Add project files.

---
+++ DoAnWeb/Controllers/TablesController.cs
@@ -0,0 +1,44 @@
+﻿using DoAnCoSo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnCoSo.Controllers
+{
+    public class TablesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var items = db.Tables.ToList();
+            return View(items);
+        }
+        public ActionResult Detail(string alias, int id)
+        {
+            var item = db.Tables.Find(id);
+            return View(item);
+        }
+
+        public ActionResult Space(string alias, int id)
+        {
+            var items = db.Tables.ToList();
+            if (id > 0)
+            {
+                items = items.Where(x => x.spaceid == id).ToList();
+            }
+            var cate = db.Spaces.Find(id);
+            if(cate != null)
+            {
+                ViewBag.CateName = cate.title;
+            }
+            ViewBag.CateId = id;
+            return View(items);
+        }
+      
+    }
+
+}
---
+++ DoAnWeb/Models/OrderViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DoAnCoSo.Models
+{
+    public class OrderViewModel
+    {
+        [Required(ErrorMessage = "Tên khách hàng không được để trống")]
+        public string customername { get; set; }
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
+        public string phone { get; set; }
+        [Required(ErrorMessage = "Địa chỉ không được để trống")]
+        public string address { get; set; }
+        [Required(ErrorMessage = "Ngày đặt bàn không được để trống")]
+        public DateTime datetime { get; set; }
+        public string email { get; set; }
+        public int typepayment { get; set; }
+
+    }
+}
