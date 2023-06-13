d08a08a3a657cab4b76b1a2f8b9bdff2583d8c1b
nguyễn hoàng <hoangsino2002@gmail.com>
2023-06-13 10:24:20 +0700

    Thêm nhân viên

---
+++ DoAnWeb/Models/EF/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnCoSo.Models.EF
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [Required(ErrorMessage = "Bạn không thể trống tên nhân viên")]
+        [StringLength(150)]
+        public string name { get; set; }
+        public string image { get; set; }
+        public string gioitinh { get; set; }
+        public DateTime ngaysinh { get; set; }
+        public string chucvu { get; set; }
+ 
+    }
+}
